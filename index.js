bc813386991ab450108e48f643c9e85d77f11432
xfnw <xfnw@riseup.net>
2022-02-17 23:20:16 -0500

    simple chathistory and darken joins

--- index.js
+++ index.js
@@ -7,24 +7,37 @@ var humanhash = new (require("humanhash"))();
 
 var salt = "dfvd"
 
+var chathistory = [];
+
 //app.get('/', function(req, res){
 //  res.sendFile(__dirname + '/index.html');
 //});
 app.use(express.static("public"));
 
 io.on("connection", function(socket) {
-  io.emit("chat message", humanhash.humanize(socket.request.connection.remoteAddress+salt)+'-'+socket.id.substring(0, 1)+' has joined')
+  chathistory.forEach( function (msg) {
+    socket.emit("chat message", msg);
+  });
+  io.emit("chat jpq", new Date().toISOString().slice(11,19) + " " + humanhash.humanize(socket.request.connection.remoteAddress+salt, words = 1)+'-'+socket.id.substring(0, 1)+' has joined')
+  socket.on("disconnect", function(reason) {
+    io.emit("chat jpq", new Date().toISOString().slice(11,19) + " " + humanhash.humanize(socket.request.connection.remoteAddress+salt, words = 1)+'-'+socket.id.substring(0, 1)+' has left')
+  });
   socket.on("chat message", function(msg) {
     if (msg != "") {
       var ip = socket.request.connection.remoteAddress;
       msg =
-        "<" +
-        humanhash.humanize(ip + salt) +
+        new Date().toISOString().slice(11,19) +
+        " <" +
+        humanhash.humanize(ip + salt, words = 1) +
         "-" +
         socket.id.substring(0, 1) +
         "> " +
         msg;
       console.log(ip, msg);
+      chathistory.push(msg);
+      if (chathistory.length > 100) {
+        chathistory.shift();
+      }
       io.emit("chat message", msg);
     }
   });
